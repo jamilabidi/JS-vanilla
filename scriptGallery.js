196f131d97d6ddc005879ea8edd3d2bb1d495097
jamil abidi <jamil.abidi@le-campus-numerique.fr>
2024-04-04 11:28:34 +0200

    generating div with img fetch from api

--- scriptGallery.js
+++ scriptGallery.js
@@ -1,9 +1,37 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+
+
     const imgFetch = fetch('https://picsum.photos/v2/list')
         .then(resp => resp.json())
         .then(json => {
-            console.log(json);
+            displayImgs(json)
+            // console.log(json);
         })
 
     console.log(imgFetch);
+
+ const imgblocks=document.getElementById("container");
+console.log(imgblocks);
+
+const displayImgs= (arrayObjects)=>{
+    console.log(arrayObjects);
+    const imgsNode=arrayObjects.map((img)=>{
+        return createImgElement(img);
+    });
+    imgblocks.append(...imgsNode);
+}
+
+const createImgElement= (img)=>{
+    const div=document.createElement("div")
+    div.innerHTML=`
+    <div>
+    <img src="${img.download_url}" style="max-width: 25rem"/>
+    </div>
+    `
+    return div;
+}
+
+
+
+
 })
