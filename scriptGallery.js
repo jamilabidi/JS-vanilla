8886055187f13d6f15c897c07af68cfd1f515a89
jamil abidi <jamil.abidi@le-campus-numerique.fr>
2024-04-04 11:05:01 +0200

    fetch api ok

--- scriptGallery.js
+++ scriptGallery.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+    const imgFetch = fetch('https://picsum.photos/v2/list')
+        .then(resp => resp.json())
+        .then(json => {
+            console.log(json);
+        })
 
-
+    console.log(imgFetch);
 })
